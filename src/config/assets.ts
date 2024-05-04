7d038492af87cf8712d6e01d6c4a979624ab9a1d
Sennett Lau <laub1199@gmail.com>
2024-05-04 21:33:47 +0800

    more banners

--- src/config/assets.ts
+++ src/config/assets.ts
@@ -3,4 +3,12 @@ export const bannerConfig = [
     name: 'retordinals_001',
     by: 'Retordinals',
   },
+  {
+    name: 'retordinals_002',
+    by: 'Retordinals',
+  },
+  {
+    name: 'retordinals_003',
+    by: 'Retordinals',
+  },
 ]
