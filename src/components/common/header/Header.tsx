e3c6942e19314d422f28eca5ea61663f96366b6c
Sennett Lau <laub1199@gmail.com>
2024-05-04 18:55:45 +0800

    updated header with text logo

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 const Header = () => {
   return (
     <header className='bg-white bg-opacity-50 fixed top-0 w-full bg-no-repeat bg-cover bg-center shadow-md z-10'>
-      <div className='flex justify-center items-center w-full max-w-[1440px] mx-auto px-6'>
-        <Image src='/logo.png' alt='logo' width={80} height={70} />
+      <div className='flex justify-center items-center w-full max-w-[1440px] mx-auto px-6 py-2'>
+        <Image src='/text_logo.png' alt='logo' width={253} height={56} />
       </div>
     </header>
   )
